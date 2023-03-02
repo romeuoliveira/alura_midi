7dff2ac3c109f947bd91235c8af3b460d101f176
Romeu Oliveira <oromeu@yahoo.com.br>
2023-03-02 23:46:43 +0000

    projeto alura midi, refazendo o repositorio para colocar no github pages

---
+++ alura_midi/main.js
@@ -0,0 +1,45 @@
+
+
+function tocaSom (seletorAudio ){
+    const elemento = document.querySelector(seletorAudio);
+
+
+    if(elemento && elemento.localName === 'audio'){
+            elemento.play(); 
+    }
+    else  {
+          //alert ('Elemento nao encontrado');
+          console.log('Elemento nao encontrado ou seletor invalido');
+    }
+
+}
+
+const listaDeTeclas  =  document.querySelectorAll('.tecla');
+
+for(contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;   //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code == 'Space'|| evento.code === 'Enter') {
+            tecla.classList.add('ativa'); 
+        } 
+
+    } 
+    
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+
+    }  
+
+}
+    
+
+
